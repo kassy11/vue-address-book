3d4bd99bcefc567846ebac79ee6fcf9fe664181c
kassy11 <kotaro11kassy@gmail.com>
2020-04-06 16:43:50 +0900

    :tada: init commit

---
+++ src/components/Addresses.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>マイアドレス帳</h1>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+    <v-list>
+      <v-list-item>
+        <v-list-item-avatar>
+          <img v-if="photoURL" :src="photoURL">
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>kassy11</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list-item v-for="(item, index) in items" :key="index" :to="item.link">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      items: [
+        { title: '連絡先一覧', icon: 'mdi-menu' }
+      ]
+    }
+  }
+}
+</script>
