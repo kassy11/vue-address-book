1f893d5f29c688c33219cf3ecf1960f2ee3d1418
kassy11 <kotaro11kassy@gmail.com>
2020-04-14 21:55:00 +0900

    :sparkles: googleログインの見た目だけ実装

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAMp1PgXko19W9iPjPr96KkOJhi5jxmf3w",
+  authDomain: "my-address-pj-57015.firebaseapp.com",
+  databaseURL: "https://my-address-pj-57015.firebaseio.com",
+  projectId: "my-address-pj-57015",
+  storageBucket: "my-address-pj-57015.appspot.com",
+  messagingSenderId: "577901911532",
+  appId: "1:577901911532:web:66a858090d193b4b7334c9",
+  measurementId: "G-ZZKFHWZ0TR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
