b59a54dcad5307de27914a13da8821e18055487c
kassy11 <kotaro11kassy@gmail.com>
2020-04-14 22:17:38 +0900

    ログアウト処理

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@
       <v-toolbar-title>マイアドレス帳</v-toolbar-title>
 
       <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text @click="logout">ログアウト</v-btn>
+      </v-toolbar-items>
 
     </v-app-bar>
     <SideNav/>
@@ -34,6 +37,8 @@ export default {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.setLoginUser(user)
+      }else{
+        this.deleteLoginUser()
       }
     })
   },
@@ -43,7 +48,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['toggleSideMenu', 'setLoginUser'])
+    ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser'])
   }
 }
 </script>
