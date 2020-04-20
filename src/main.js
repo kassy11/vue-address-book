c7e15c486702fb492f744f849425bda329cd6711
kassy11 <kotaro11kassy@gmail.com>
2020-04-20 12:40:34 +0900

    デプロイとenvの設定

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
